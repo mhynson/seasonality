e03948d90c01d8da638a80c4c1fe492c544129e7
Mike Hynson <mike@mhdigitalsolutions.com>
2024-07-06 12:22:30 -0400

    improve ButtonTab classes

--- app/src/app/components/ButtonTabs.tsx
+++ app/src/app/components/ButtonTabs.tsx
@@ -4,20 +4,33 @@ interface IButtonTabsProps {
   tabs: { label: string; key: string }[];
 }
 
-const buttonClasses = "p-4 rounded-l ";
 const buttonColor = "bg-indigo-600";
 const buttonColorActive = "bg-indigo-800";
+
+const getTabColor = (isActive: boolean): string =>
+  isActive ? buttonColorActive : buttonColor;
+
+const getRoundedClass = (index: number, length: number) => {
+  const isFirst = index === 0;
+  const isLast = index === length - 1;
+  const classNames = [];
+  if (isFirst) classNames.push("rounded-l");
+  if (isLast) classNames.push("rounded-r");
+
+  return classNames.join(" ");
+};
 
 export const ButtonTabs = (props: IButtonTabsProps) => {
   const { activeTab, setActiveTab, tabs } = props;
   return (
     <div className="text-white">
-      {tabs.map(({ label, key }) => (
+      {tabs.map(({ label, key }, idx) => (
         <button
           key={key}
-          className={`${buttonClasses} ${
-            activeTab === key ? buttonColorActive : buttonColor
-          }`}
+          className={`p-4 ${getTabColor(activeTab === key)} ${getRoundedClass(
+            idx,
+            tabs.length
+          )}`}
           onClick={() => setActiveTab(key)}
         >
           {label}
