5b765d1064c2b0b0ffd43c5aff8fd3f9a2e7d504
Mike Hynson <mike@mhdigitalsolutions.com>
2024-08-08 14:10:44 -0400

    Fix properties for JumpScrollButton

--- app/src/app/components/JumpScrollButton.tsx
+++ app/src/app/components/JumpScrollButton.tsx
@@ -31,9 +31,9 @@ export const JumpScrollButton = ({ symbol, view }: IJumpScrollButtonProps) => {
           <path
             d="M3 9h18M7 3v2M17 3v2M10 16l2 2m0 0l2-2m-2 2v-6M6.2 21h11.6c1.12 0 1.68 0 2.11-.22.38-.19.69-.5.88-.88.22-.43.22-1 .22-2.11V8.2c0-1.12 0-1.68-.22-2.11a2 2 0 0 0-.88-.88C19.48 5 18.92 5 17.8 5H6.2c-1.12 0-1.68 0-2.11.22-.38.19-.69.5-.88.88C3 6.52 3 7.08 3 8.2v9.6c0 1.12 0 1.68.22 2.11.19.38.5.69.88.88C4.52 21 5.08 21 6.2 21Z"
             stroke="#000"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </button>
