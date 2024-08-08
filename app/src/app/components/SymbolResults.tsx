418e595cdee06035df8c69463e10bd398013a0a9
Mike Hynson <mike@mhdigitalsolutions.com>
2024-08-08 14:09:57 -0400

    Refactor renderSymbolData into SymbolResults component

---
+++ app/src/app/components/SymbolResults.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { getError } from "../api/seasonality/utils";
+import { TSymbolGroupedData, TSymbolSeasonalityDataView } from "../types";
+import { JumpScrollButton } from "./JumpScrollButton";
+import { ErrorMessage } from "./ErrorMessage";
+import { SeasonalityBars } from "./SeasonalityBars";
+
+interface ISymbolResults {
+  symbol: string;
+  symbolData: TSymbolSeasonalityDataView;
+  setData: (data: TSymbolGroupedData) => void;
+}
+
+export const SymbolResults: React.FC<ISymbolResults> = ({
+  symbol,
+  symbolData,
+  setData,
+}) => {
+  const { view, data } = symbolData;
+  const error = getError(data);
+
+  const btnBaseClasses =
+    "rounded px-4 py-2 ml-1 text-white hover:bg-red-300 hover:opacity-100";
+  const btnActiveClasses = "bg-red-400";
+  const btnInactiveClasses = "bg-slate-300 opacity-70";
+
+  const dataForView = data.find(({ timeframe }) => timeframe === view);
+
+  return (
+    <div className="mt-8 border-t-2 py-4">
+      <div className="stock-header mb-4 p-4" id={`${symbol}--stock-header`}>
+        <h2 className="text-xl font-bold">{symbol.toUpperCase()}</h2>
+        {!error ? (
+          <>
+            <div className="flex justify-center">
+              <button
+                className={`${btnBaseClasses} ${
+                  view === "monthly" ? btnActiveClasses : btnInactiveClasses
+                }`}
+                onClick={() =>
+                  setData((prevData: TSymbolGroupedData) => {
+                    const newData = {
+                      ...prevData,
+                      [symbol]: { ...prevData[symbol], view: "monthly" },
+                    };
+                    return newData;
+                  })
+                }
+              >
+                Monthly View
+              </button>
+              <button
+                className={`${btnBaseClasses} ${
+                  view === "weekly" ? btnActiveClasses : btnInactiveClasses
+                }`}
+                onClick={() =>
+                  setData((prevData: TSymbolGroupedData) => {
+                    return {
+                      ...prevData,
+                      [symbol]: { ...prevData[symbol], view: "weekly" },
+                    };
+                  })
+                }
+              >
+                Weekly View
+              </button>
+            </div>
+            <JumpScrollButton symbol={symbol} view={view} />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      {error ? (
+        <div className="my-4 p-4 bg-slate-50">
+          <pre>
+            <ErrorMessage error={error} />
+          </pre>
+        </div>
+      ) : (
+        <>
+          <div className="bg-white p-4 rounded mt-4">
+            <SeasonalityBars seasonalityData={dataForView} symbol={symbol} />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,20 +3,14 @@
 import { useContext, useState } from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
-import { cleanSymbolList, getError } from "./api/seasonality/utils";
+import { cleanSymbolList } from "./api/seasonality/utils";
 import { LINKS as links } from "./constants";
-import { ErrorMessage } from "./components/ErrorMessage";
-import { SeasonalityBars } from "./components/SeasonalityBars";
 import { GlobalNav } from "./components/GlobalNav";
 import { TickerSymbolForm } from "./components/TickerSymbolForm";
-import {
-  TSeasonalityData,
-  TSymbolGroupedData,
-  TSymbolSeasonalityDataView,
-} from "./types";
-import { JumpScrollButton } from "./components/JumpScrollButton";
+import { TSymbolGroupedData } from "./types";
 import { YearsSelector } from "./components/YearsSelector";
 import { YearsContext, YearsContextType } from "./context/YearsContext";
+import { SymbolResults } from "./components/SymbolResults";
 
 const Home = () => {
   const [symbols, setSymbols] = useState("");
@@ -41,85 +35,6 @@ const Home = () => {
     setData(allData);
   };
 
-  const renderSymbolData = (
-    symbol: string,
-    symbolData: TSymbolSeasonalityDataView
-  ) => {
-    const { view, data } = symbolData;
-    const error = getError(data);
-
-    const btnBaseClasses =
-      "rounded px-4 py-2 ml-1 text-white  hover:bg-red-300 hover:opacity-100";
-    const btnActiveClasses = "bg-red-400";
-    const btnInactiveClasses = "bg-slate-300 opacity-70";
-
-    const dataForView = data.find(
-      ({ timeframe }: TSeasonalityData) => timeframe === view
-    );
-
-    return (
-      <div key={symbol} className="mt-8 border-t-2 py-4">
-        <div
-          className="stock-header mb-4p
-        "
-          id={`${symbol}--stock-header`}
-        >
-          <h2 className="text-xl font-bold">{symbol.toUpperCase()}</h2>
-
-          {!error ? (
-            <>
-              <div className="flex justify-center">
-                <button
-                  className={`${btnBaseClasses} ${
-                    view === "monthly" ? btnActiveClasses : btnInactiveClasses
-                  }`}
-                  onClick={() =>
-                    setData((prevData) => ({
-                      ...prevData,
-                      [symbol]: { ...prevData[symbol], view: "monthly" },
-                    }))
-                  }
-                >
-                  Monthly View
-                </button>
-                <button
-                  className={`${btnBaseClasses} ${
-                    view === "weekly" ? btnActiveClasses : btnInactiveClasses
-                  }`}
-                  onClick={() =>
-                    setData((prevData) => ({
-                      ...prevData,
-                      [symbol]: { ...prevData[symbol], view: "weekly" },
-                    }))
-                  }
-                >
-                  Weekly View
-                </button>
-              </div>
-              <JumpScrollButton symbol={symbol} view={view} />
-            </>
-          ) : (
-            <></>
-          )}
-        </div>
-
-        {error ? (
-          <div className="my-4 p-4 bg-slate-50">
-            <pre>
-              <ErrorMessage error={error} />
-            </pre>
-          </div>
-        ) : (
-          <>
-            <div className="bg-white p-4 rounded mt-4">
-              <SeasonalityBars seasonalityData={dataForView} symbol={symbol} />
-            </div>
-          </>
-        )}
-      </div>
-    );
-  };
-
   return (
     <>
       <GlobalNav links={links} />
@@ -137,17 +52,20 @@ const Home = () => {
           </p>
         </div>
         <div className="container xl mt-9 mx-auto p-12 bg-slate-700 rounded-xl shadow-lg text-white">
+          <YearsSelector years={years} />
           <TickerSymbolForm
             handleSubmit={handleSubmit}
             onTextChange={(e) => setSymbols(e.target.value)}
             symbols={symbols}
           />
-          <YearsSelector years={years} />
           {Object.keys(data).length > 0 && (
             <div>
-              {Object.entries(data).map(([symbol, symbolData]) =>
-                renderSymbolData(symbol, symbolData)
-              )}
+              {Object.entries(data).map(([symbol, symbolData]) => (
+                <SymbolResults
+                  key={symbol}
+                  {...{ symbol, symbolData, setData }}
+                />
+              ))}
             </div>
           )}
         </div>
